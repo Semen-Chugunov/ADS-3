f1e69c65d54312cc844b65100735a10b2004d90c
Semen-Chugunov <125124591+Semen-Chugunov@users.noreply.github.com>
2023-03-11 16:30:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int l = 0;
+  int r = size - 1;
+  int c = 0;
+  while (l <= r) {
+    int m = (l + r) / 2;
+    if (arr[m] == value) {
+      int i = m - 1;
+      c++;
+      while (i >= l && arr[i] == value) {
+        c++;
+        i--;
+      }
+      i = m + 1;
+      while (i <= r && arr[i] == value) {
+        c++;
+        i++;
+      }
+    } else if (arr[m] > value) {
+      r = m - 1;
+    } else {
+      l = m + 1;
+    }
+ }
+  return c;
 }
